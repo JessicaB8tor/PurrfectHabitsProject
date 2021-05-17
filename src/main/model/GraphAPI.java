f04b0ec916f97af71964c74d59113bf0b116c51b
jkbator <jkbator333@gmail.com>
2021-05-16 22:29:46 -0700

    Added Basic Graph Template

--- src/main/model/GraphAPI.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-import java.net.HttpURLConnection;
-import java.net.URL;
-
-public class GraphAPI {
-    private static HttpURLConnection connection;
-    //URL url = new URL("");
-}
