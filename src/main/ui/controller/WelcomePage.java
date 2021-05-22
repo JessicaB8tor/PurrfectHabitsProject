b45d22750b836ac143a301321955a32bb450f014
jkbator <jkbator333@gmail.com>
2021-05-22 14:00:45 -0700

    Welcome Page Again

--- src/main/ui/controller/WelcomePage.java
+++ src/main/ui/controller/WelcomePage.java
@@ -1,62 +1,35 @@
 package ui.controller;
 
 import javafx.application.Application;
-import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+
 
 public class WelcomePage extends Application {
-    BorderPane borderPane;
-    VBox leftVBox;
-    VBox rightVBox;
-    HBox bottomHBox;
+    GridPane gridPane;
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        borderPane = new BorderPane();
-        borderPane.setPrefSize(1000, 500);
-        borderPane.setStyle("-fx-background-color: #86D0F7");
-
-        createLeftVBox();
-        createRightVBox();
-        createBottomHBox();
-
-        Scene root = new Scene(borderPane);
-        primaryStage.setScene(root);
-        primaryStage.show();
-    }
-
-    public void createLeftVBox() throws FileNotFoundException {
-        leftVBox = new VBox();
-        leftVBox.setAlignment(Pos.CENTER);
+        gridPane = new GridPane();
+        gridPane.setPrefSize(1000, 500);
+        gridPane.setAlignment(Pos.CENTER);
+        gridPane.setStyle("-fx-background-color: #86D0F7");
 
         //Image Stuff
         FileInputStream stream = new FileInputStream("data/misc/catLogo.png");
         Image image = new Image(stream);
         ImageView logo = new ImageView(image);
-        logo.setFitWidth(333);
+        logo.setFitWidth(300);
         logo.setPreserveRatio(true);
-
-        VBox.setMargin(logo, new Insets(70, 0, 0, 50));
-        leftVBox.getChildren().add(logo);
-        borderPane.setLeft(leftVBox);
-    }
-
-    public void createRightVBox() {
-        rightVBox = new VBox();
-        rightVBox.setAlignment(Pos.CENTER);
 
         //Label Stuff
         Label welcomeTo = new Label("WELCOME TO");
@@ -64,23 +37,20 @@ public class WelcomePage extends Application {
         welcomeTo.setFont(new Font("Showcard Gothic", 50));
         purrfectHabits.setFont(new Font("Showcard Gothic", 50));
 
-        VBox.setMargin(welcomeTo, new Insets(0, 100, 0, 0));
-        VBox.setMargin(purrfectHabits, new Insets(0, 100, 0, 0));
-        rightVBox.getChildren().addAll(welcomeTo, purrfectHabits);
-        borderPane.setRight(rightVBox);
-    }
-
-    public void createBottomHBox() {
-        bottomHBox = new HBox();
-        bottomHBox.setAlignment(Pos.CENTER_RIGHT);
-
         //Button Stuff
         Button button = new Button("Get Started");
-        button.setFont(new Font("Dosis Bold", 25));
+        button.setFont(new Font("Dosis Bold", 20));
         button.setStyle("-fx-background-color: #FFCB3D");
 
-        HBox.setMargin(button, new Insets(0, 240,125, 0));
-        bottomHBox.getChildren().add(button);
-        borderPane.setBottom(bottomHBox);
+        gridPane.add(logo, 0, 0, 1, 3);
+        gridPane.add(welcomeTo, 1, 0);
+        gridPane.add(purrfectHabits, 1, 1);
+        gridPane.add(button, 1, 2);
+
+
+        Scene root = new Scene(gridPane);
+        primaryStage.setScene(root);
+        primaryStage.show();
     }
 }
+
