e0d60979515876209ec76dcb6cbea81ccce6df14
jkbator <jkbator333@gmail.com>
2021-05-21 23:14:02 -0700

    Welcome Page

--- src/main/ui/controller/NewAccountPage.java
+++ src/main/ui/controller/NewAccountPage.java
@@ -1,7 +1,9 @@
 package ui.controller;
 
+import javafx.geometry.Insets;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.*;
@@ -42,15 +44,9 @@ public class NewAccountPage {
     }
 
     public void createCenterGridPane() {
+        //TODO: Make method smaller for checkstyle & readability
         GridPane gridPane = new GridPane();
         gridPane.setAlignment(Pos.CENTER);
-
-        //Set Dimensions
-        ColumnConstraints col1 = new ColumnConstraints();
-        col1.setPercentWidth(25);
-        ColumnConstraints col2 = new ColumnConstraints();
-        col2.setPercentWidth(25);
-        gridPane.getColumnConstraints().addAll(col1, col2);
 
 
         //Label Stuff
@@ -69,18 +65,33 @@ public class NewAccountPage {
         nameField.setPromptText("ex. Jessica");
         TextField emailField = new TextField();
         emailField.setPromptText("ex. meow@gmail.com");
+        TextField passwordField = new TextField();
+        TextField confirmPasswordField = new TextField();
 
         gridPane.add(name, 0, 0);
         gridPane.add(nameField, 1, 0);
-        gridPane.add(emailAddress, 2, 1);
+        gridPane.add(emailAddress, 0, 1);
         gridPane.add(emailField, 1, 1);
+        gridPane.add(password, 0, 2);
+        gridPane.add(passwordField, 1, 2);
+        gridPane.add(confirmPassword, 0, 3);
+        gridPane.add(confirmPasswordField, 1, 3);
+
         borderPane.setCenter(gridPane);
     }
 
     public void createBottomVBox() {
         VBox bottomVBox = new VBox();
+        bottomVBox.setAlignment(Pos.CENTER);
 
+
+        //Button Stuff
+        Button button = new Button("Submit");
+        button.setFont(new Font("Dosis Bold", 20));
+        button.setStyle("-fx-background-color: #86D0F7");
+        VBox.setMargin(button, new Insets(10, 10, 80, 10));
+
+        bottomVBox.getChildren().add(button);
         borderPane.setBottom(bottomVBox);
-
     }
 }
